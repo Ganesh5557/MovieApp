cad2c64b4e35bd85999ef7dece2e645907edea19
Ganesh5557 <ganesh.pandey5557@gmail.com>
2020-04-18 18:09:04 -0400

    Updated  URI keys

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -4,7 +4,7 @@ export const USER_SERVER = '/api/users';
 
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+export const API_KEY = 'bb04f11ecee966dc20375adb5c3952f9';
 
 
 export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
